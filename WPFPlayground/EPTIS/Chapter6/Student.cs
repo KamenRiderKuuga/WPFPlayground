82a85b5efc88046aad5a376d2b9d723973effacf
GadHao <39508075+GadHao@users.noreply.github.com>
2021-01-25 23:44:54 +0800

    更新更新深入浅出WPF第6章的学习内容

---
+++ WPFPlayground/EPTIS/Chapter6/Student.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace EPTIS.Chapter6
+{
+    public class Student : INotifyPropertyChanged
+    {
+        private string _name;
+
+        /// <summary>
+        /// 学生姓名
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                this.PropertyChanged.Invoke(this,new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
