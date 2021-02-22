2a0838c9202e7b2045694cc6309c907a15c4eec9
GadHao <39508075+GadHao@users.noreply.github.com>
2021-02-23 00:09:21 +0800

    更新与Blinding的Path有关的学习内容

--- WPFPlayground/EPTIS/Chapter6Window.xaml.cs
+++ WPFPlayground/EPTIS/Chapter6Window.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using EPTIS.Chapter6;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,12 +25,37 @@ namespace EPTIS
             binding.Path = new PropertyPath("Name");
 
             BindingOperations.SetBinding(textBoxName, TextBox.TextProperty, binding);
-             
+
             // 继承于FrameworkElement的UI元素类型，比如TextBox还可以写成
             textBoxName.SetBinding(TextBox.TextProperty, binding);
 
             // 或者简写成
-            textBoxName.SetBinding(TextBox.TextProperty, new Binding("Name") { Source = _student = new Student()});
+            textBoxName.SetBinding(TextBox.TextProperty, new Binding("Name") { Source = _student = new Student() });
+
+            textboxWithNotify.SourceUpdated += TextboxSourceUpdate_Event;
+            textboxWithNotify.TargetUpdated += TextboxTargetUpdate_Event;
+            textBoxBindingWithCode.SetBinding(TextBox.TextProperty, new Binding("Text.Length") { Source = textBoxBeBinding, Mode = BindingMode.OneWay });
+            textBoxBeBinding.SetBinding(TextBox.TextProperty, new Binding("/[2]") { Source = new List<string>() { "Tim", "Tom", "Blog" }, Mode = BindingMode.OneWay });
+        }
+
+        /// <summary>
+        /// 在Binding源通知绑定了源的属性更新之后触发的事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextboxTargetUpdate_Event(object sender, DataTransferEventArgs e)
+        {
+            MessageBox.Show("绑定了Binding源的目标属性已被更新");
+        }
+
+        /// <summary>
+        /// 在Binding源收到通知，对应值发生改变之后会触发的事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextboxSourceUpdate_Event(object sender, DataTransferEventArgs e)
+        {
+            MessageBox.Show("Binding源被更新了");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
