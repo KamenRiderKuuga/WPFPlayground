dd6b54135bb3f48ab2f7be6d277ad19fd49fb4fd
GadHao <39508075+GadHao@users.noreply.github.com>
2021-03-11 23:51:46 +0800

    Create Calculator.cs

---
+++ WPFPlayground/EPTIS/Chapter6/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace EPTIS.Chapter6
+{
+    public class Calculator
+    {
+        public string Add(string arg1, string arg2)
+        {
+            double x = 0;
+            double y = 0;
+            double z = 0;
+            if (double.TryParse(arg1, out x) && double.TryParse(arg2, out y))
+            {
+                return (x + y).ToString();
+            }
+
+            return "Input Error";
+        }
+    }
+}
