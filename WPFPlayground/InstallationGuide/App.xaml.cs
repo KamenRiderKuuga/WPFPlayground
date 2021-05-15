4c5e6e4138cef26030df0c13b18ad80ab50ad7d0
GadHao <39508075+GadHao@users.noreply.github.com>
2021-05-15 23:57:37 +0800

    feat: new project with ReactiveUI demo

---
+++ WPFPlayground/InstallationGuide/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ReactiveUI;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InstallationGuide
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // A helper method that will register all classes that derive off IViewFor 
+            // into our dependency injection container. ReactiveUI uses Splat for it's 
+            // dependency injection by default, but you can override this if you like.
+            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+        }
+    }
+}
